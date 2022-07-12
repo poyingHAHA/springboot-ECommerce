ae3c0d0a1ec2ea078508f4da47f59d6a8f09404f
poyingHAHA <poying.code@gmail.com>
2022-07-12 22:04:41 +0800

    訂單功能 - 檢查user是否存在、扣除商品庫存

--- springboot-Ecommerce/src/main/java/com/poying/springbootecommerce/dao/impl/ProductDaoImpl.java
+++ springboot-Ecommerce/src/main/java/com/poying/springbootecommerce/dao/impl/ProductDaoImpl.java
@@ -137,6 +137,19 @@ public class ProductDaoImpl implements ProductDao {
         return total;
     }
 
+    @Override
+    public void updateStock(Integer productId, Integer stock) {
+        String sql = "UPDATE product SET stock = :stock, last_modified_date = :lastModifiedDate" +
+                " WHERE product_id = :productId";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("productId", productId);
+        map.put("stock", stock);
+        map.put("lastModifiedDate", new Date());
+
+        namedParameterJdbcTemplate.update(sql, map);
+    }
+
     private String addFilteringSql(String sql,
                                    Map<String, Object> map,
                                    ProductQueryParams productQueryParams){
--- springboot-Ecommerce/src/main/java/com/poying/springbootecommerce/service/impl/OrderServiceImpl.java
+++ springboot-Ecommerce/src/main/java/com/poying/springbootecommerce/service/impl/OrderServiceImpl.java
@@ -2,15 +2,21 @@ package com.poying.springbootecommerce.service.impl;
 
 import com.poying.springbootecommerce.dao.OrderDao;
 import com.poying.springbootecommerce.dao.ProductDao;
+import com.poying.springbootecommerce.dao.UserDao;
 import com.poying.springbootecommerce.dto.BuyItem;
 import com.poying.springbootecommerce.dto.CreateOrderRequest;
 import com.poying.springbootecommerce.model.Order;
 import com.poying.springbootecommerce.model.OrderItem;
 import com.poying.springbootecommerce.model.Product;
+import com.poying.springbootecommerce.model.User;
 import com.poying.springbootecommerce.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,20 +24,43 @@ import java.util.List;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
     @Autowired
     private OrderDao orderDao;
-
+    @Autowired
+    private UserDao userDao;
     @Autowired
     private ProductDao productDao;
 
     @Transactional // 復原已經執行的資料庫操作，讓資料庫操作要嘛失敗要嘛成功
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        // 檢查user是否存在
+        User user = userDao.getUserById(userId);
+
+        if(user == null){
+            log.warn("該使用者{}不存在", userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         int totalAmount = 0;
         List<OrderItem> orderItemList = new ArrayList<>();
 
         for(BuyItem buyItem : createOrderRequest.getBuyItemList()){
             Product product = productDao.getProductById(buyItem.getProductId());
+
+            // 檢查商品是否存在、庫存是否足夠
+            if(product == null){
+                log.warn("商品 {} 不存在", buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }else if(product.getStock() < buyItem.getQuantity()){
+                log.warn("商品 {} 庫存數量不足，無法購買。剩餘庫存 {}，欲購買數量 {}",
+                        buyItem.getProductId(), product.getStock(), buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            //扣除商品庫存
+            productDao.updateStock(product.getProductId(),product.getStock() - buyItem.getQuantity());
 
             // 計算總價錢
             int amount = buyItem.getQuantity() * product.getPrice();
@@ -46,6 +75,7 @@ public class OrderServiceImpl implements OrderService {
             orderItemList.add(orderItem);
         }
 
+        // 創建訂單
         Integer orderId = orderDao.createOrder(userId, totalAmount);
 
         orderDao.createOrderItems(orderId, orderItemList);
